1fef475c970a60a2a2cc12376409646420ee15b7
Alex <alvastarua@gmail.com>
2021-12-26 10:59:25 +0200

    Create index.php

---
+++ coursework/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$title = 'Главная страница';
+
+require __DIR__ . '/templates/header.php';
+
+?>
+
+<!-- Здесь будет смесь HTML-кода и PHP -->
+
+<?php
+
+require __DIR__ .'/templates/footer.php';
